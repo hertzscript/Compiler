233cbf814c42a4d323c7b5845111c30b1384996c
Floofies <fantasticdragons@gmail.com>
2019-01-06 23:54:29 -0500

    Fixed dispatcher import

--- src/header.js
+++ src/header.js
@@ -1,4 +1,4 @@
 var global = typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : typeof window !== 'undefined' ? window : {};
-const hzDispatcher = require("./Dispatcher.js");
+const hzDispatcher = require("hertzscript-dispatcher");
 const hzDisp = new hzDispatcher(("hzTknLib" in global) ? global.hzTknLib : null);
 if (!("hzTknLib" in global)) global.hzTknLib = hzDisp.tokenLib;
